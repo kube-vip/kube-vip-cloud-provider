5ecd7fae0541b83cc131c4126864ceb6e5b3382a
lubronzhan <lubronzhan@gmail.com>
2024-03-03 18:23:27 -0800

    Group kube-vip-cloud-provider
    Signed-off-by: lubronzhan <lubronzhan@gmail.com>

--- test/e2e/defaultconfig/default_config_test.go
+++ test/e2e/defaultconfig/default_config_test.go
@@ -11,7 +11,9 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/require"
+	api_errors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 
 	tu "github.com/kube-vip/kube-vip-cloud-provider/pkg/testutil"
 	"github.com/kube-vip/kube-vip-cloud-provider/test/e2e"
@@ -26,7 +28,7 @@ func TestDeployWithDefaultConfig(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	// By default, configMap only contains below 3 cidr
+	// By default, configMap only contains below 3 cidr:
 	// cidr-default: 192.168.0.200/29
 	// cidr-plunder: 192.168.0.210/29
 	// cidr-testing: 192.168.0.220/29
@@ -39,7 +41,7 @@ var _ = AfterSuite(func() {
 	require.NoError(f.T(), f.Deployment.DeleteResources())
 })
 
-var watchedNamespace = "testing"
+var watchedNamespaces = []string{"default", "testing", "plunder"}
 
 var _ = Describe("Default config", func() {
 	Context("Deploy service in namespace that kube-vip-cloud-provider is configured and is not configured", func() {
@@ -60,20 +62,33 @@ var _ = Describe("Default config", func() {
 					return !e2e.ServiceIsReconciled(svc) && !e2e.ServiceHasIPAssigned(svc)
 				}, 30*time.Second, time.Second, fmt.Sprintf("Service is not supposed to have label or annotation %v, with error %v", svc, err))
 
-				By("Create a service type LB in testing namespace")
-				svc = tu.NewService("test2", tu.TweakNamespace("watchedNamespace"))
-				_, err = f.Client.CoreV1().Services(svc.Namespace).Create(ctx, svc, meta_v1.CreateOptions{})
-				require.NoError(f.T(), err)
+				for _, ns := range watchedNamespaces {
+					By("Create a service type LB in watched namespace")
+					svc = tu.NewService(fmt.Sprintf("test-%s", ns), tu.TweakNamespace(ns))
+					_, err = f.Client.CoreV1().Services(svc.Namespace).Create(ctx, svc, meta_v1.CreateOptions{})
+					require.NoError(f.T(), err)
 
-				By("Service should have a valid IP assigned, and related kube-vip annotations and labels")
-				require.Eventually(f.T(), func() bool {
-					svc, err = f.Client.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, meta_v1.GetOptions{})
-					if err != nil {
+					By("Service should have a valid IP assigned, and related kube-vip annotations and labels")
+					require.Eventually(f.T(), func() bool {
+						svc, err = f.Client.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, meta_v1.GetOptions{})
+						if err != nil {
+							return false
+						}
+						return e2e.ServiceIsReconciled(svc) && e2e.ServiceHasIPAssigned(svc)
+					}, 30*time.Second, time.Second, fmt.Sprintf("Service is not successfully reconciled %v, with error %v", svc, err))
+
+					By("Clean up the service")
+					err := f.Client.CoreV1().Services(ns).Delete(context.TODO(), svc.Name,
+						meta_v1.DeleteOptions{PropagationPolicy: ptr.To(meta_v1.DeletePropagationBackground)})
+					require.Eventually(f.T(), func() bool {
+						svc, err = f.Client.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, meta_v1.GetOptions{})
+						if api_errors.IsNotFound(err) {
+							return true
+						}
 						return false
-					}
-					return e2e.ServiceIsReconciled(svc) && e2e.ServiceHasIPAssigned(svc)
-				}, 30*time.Second, time.Second, fmt.Sprintf("Service is not successfully reconciled %v, with error %v", svc, err))
+					}, 30*time.Second, time.Second, fmt.Sprintf("Service is not successfully deleted %v, with error %v", svc, err))
+				}
 			})
-		}, watchedNamespace)
+		}, watchedNamespaces[1:]...) // Don't delete default namespace.
 	})
 })
