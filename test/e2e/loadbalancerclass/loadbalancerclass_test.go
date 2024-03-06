25da900e62d4a7a384d3392fdf4fbe2479a7d243
Lubron <lubronzhan@gmail.com>
2024-03-06 11:32:17 -0800

    Add e2e Test loadbalancerclass (#131)
    Signed-off-by: lubronzhan <lubronzhan@gmail.com>

--- test/e2e/loadbalancerclass/loadbalancerclass_test.go
+++ test/e2e/loadbalancerclass/loadbalancerclass_test.go
@@ -1,6 +1,6 @@
 //go:build e2e
 
-package defaultconfig
+package loadbalancerclass
 
 import (
 	"context"
@@ -11,10 +11,12 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/require"
+	core_v1 "k8s.io/api/core/v1"
 	api_errors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 
+	"github.com/kube-vip/kube-vip-cloud-provider/pkg/provider"
 	tu "github.com/kube-vip/kube-vip-cloud-provider/pkg/testutil"
 	"github.com/kube-vip/kube-vip-cloud-provider/test/e2e"
 )
@@ -22,9 +24,9 @@ import (
 // Each suite load default manifest from scratch, so that changes on manifest objects won't impact other tests suites.
 var f = e2e.NewFramework()
 
-func TestDeployWithDefaultConfig(t *testing.T) {
+func TestDeployWithLoadBalancerClass(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "deploy with default config")
+	RunSpecs(t, "deploy with loadbalancerclass")
 }
 
 var _ = BeforeSuite(func() {
@@ -33,6 +35,13 @@ var _ = BeforeSuite(func() {
 	// cidr-plunder: 192.168.0.210/29
 	// cidr-testing: 192.168.0.220/29
 
+	// enable loadbalancerclass
+	f.Deployment.Deployment.Spec.Template.Spec.Containers[0].Env = append(f.Deployment.Deployment.Spec.Template.Spec.Containers[0].Env,
+		core_v1.EnvVar{
+			Name:  provider.EnableLoadbalancerClassEnvKey,
+			Value: "true",
+		},
+	)
 	require.NoError(f.T(), f.Deployment.EnsureResources())
 })
 
@@ -43,13 +52,13 @@ var _ = AfterSuite(func() {
 
 var watchedNamespaces = []string{"default", "testing", "plunder"}
 
-var _ = Describe("Default config", func() {
-	Context("Deploy service in namespaces that kube-vip-cloud-provider is configured to watch and is not configured to watch", func() {
-		f.NamespacedTest("create-services-in-different-namespace", func(namespace string) {
+var _ = Describe("Loadbalancerclass enabled", func() {
+	Context("Deploy service with loadbalancerclass in different namespaces that kube-vip-cloud-provider is configured to watch and is not configured to watch", func() {
+		f.NamespacedTest("create-services-in-different-namespace-with-loadblaancerclass", func(namespace string) {
 			Specify("Service not be reconcile if namespace is not configured namespace testing, service in default namespace should be reconciled", func() {
 				ctx := context.TODO()
 				By("Create a service type LB in namespace that's not testing")
-				svc := tu.NewService("test1", tu.TweakNamespace(namespace))
+				svc := tu.NewService("test1", tu.TweakNamespace(namespace), tu.TweakAddLBClass(ptr.To(provider.LoadbalancerClass)))
 				_, err := f.Client.CoreV1().Services(svc.Namespace).Create(ctx, svc, meta_v1.CreateOptions{})
 				require.NoError(f.T(), err)
 
@@ -64,7 +73,7 @@ var _ = Describe("Default config", func() {
 
 				for _, ns := range watchedNamespaces {
 					By("Create a service type LB in watched namespace")
-					svc = tu.NewService(fmt.Sprintf("test-%s", ns), tu.TweakNamespace(ns))
+					svc = tu.NewService(fmt.Sprintf("test-%s", ns), tu.TweakNamespace(ns), tu.TweakAddLBClass(ptr.To(provider.LoadbalancerClass)))
 					_, err = f.Client.CoreV1().Services(svc.Namespace).Create(ctx, svc, meta_v1.CreateOptions{})
 					require.NoError(f.T(), err)
 
@@ -76,6 +85,40 @@ var _ = Describe("Default config", func() {
 						}
 						return e2e.ServiceIsReconciled(svc) && e2e.ServiceHasIPAssigned(svc)
 					}, 30*time.Second, time.Second, fmt.Sprintf("Service is not successfully reconciled %v, with error %v", svc, err))
+
+					By("Clean up the service")
+					err = f.Client.CoreV1().Services(ns).Delete(context.TODO(), svc.Name,
+						meta_v1.DeleteOptions{PropagationPolicy: ptr.To(meta_v1.DeletePropagationBackground)})
+					require.Eventually(f.T(), func() bool {
+						svc, err = f.Client.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, meta_v1.GetOptions{})
+						if api_errors.IsNotFound(err) {
+							return true
+						}
+						return false
+					}, 30*time.Second, time.Second, fmt.Sprintf("Service is not successfully deleted %v, with error %v", svc, err))
+				}
+			})
+		}, watchedNamespaces[1:]...) // Don't delete default namespace.
+	})
+
+	Context("Deploy service without loadbalancerclass in different namespaces that kube-vip-cloud-provider is configured to watch", func() {
+		f.NamespacedTest("create-services-namespace-without-loadblaancerclass", func(namespace string) {
+			Specify("Service not be reconcile if service doesn't have loadbalancer class even though namespace is configured to be watched", func() {
+				ctx := context.TODO()
+
+				for _, ns := range watchedNamespaces {
+					By("Create a service type LB in watched namespace")
+					svc := tu.NewService("test1", tu.TweakNamespace(ns))
+					_, err := f.Client.CoreV1().Services(svc.Namespace).Create(ctx, svc, meta_v1.CreateOptions{})
+					require.NoError(f.T(), err)
+					By("Service should not have IP assigned, it shouldn't have kube-vip annotations and labels")
+					require.Eventually(f.T(), func() bool {
+						svc, err = f.Client.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, meta_v1.GetOptions{})
+						if err != nil {
+							return false
+						}
+						return !e2e.ServiceIsReconciled(svc) && !e2e.ServiceHasIPAssigned(svc)
+					}, 30*time.Second, time.Second, fmt.Sprintf("Service is not supposed to have label or annotation %v, with error %v", svc, err))
 
 					By("Clean up the service")
 					err = f.Client.CoreV1().Services(ns).Delete(context.TODO(), svc.Name,
