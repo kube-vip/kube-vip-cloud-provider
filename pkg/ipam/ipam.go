kube-vip/kube-vip-cloud-provider

commit 7b00941f6efb55edaacbc7d1c4befb63861aa75c

Use go mod 1.22 way of range over integer syntax (#120)
Signed-off-by: lubronzhan <lubronzhan@gmail.com>

pkg/ipam/ipam.go
@@ -155,8 +155,8 @@ func FindAvailableHostFromCidr(namespace, cidr string, inUseIPSet *netipx.IPSet,
 func FindFreeAddress(poolIPSet *netipx.IPSet, inUseIPSet *netipx.IPSet, descOrder bool) (netip.Addr, error) {
 	if descOrder {
 		ipranges := poolIPSet.Ranges()
-		for i := len(ipranges) - 1; i >= 0; i-- {
-			iprange := ipranges[i]
+		for i := range len(ipranges) - 1 {
+			iprange := ipranges[len(ipranges)-1-i]
 			ip := iprange.To()
 			for {
 				if !inUseIPSet.Contains(ip) && (!ip.Is4() || !isNetworkIDOrBroadcastIP(ip.As4())) {

commit e3c2bafb185ae70d17f4c91be14d801b78def057

fix bug introduced by commit 7b00941f6efb55edaacbc7d1c4befb63861aa75c
Signed-off-by: Thomas Schätzlein <thomas@pnxs.de>

pkg/ipam/ipam.go
@@ -155,7 +155,7 @@ func FindAvailableHostFromCidr(namespace, cidr string, inUseIPSet *netipx.IPSet,
 func FindFreeAddress(poolIPSet *netipx.IPSet, inUseIPSet *netipx.IPSet, descOrder bool) (netip.Addr, error) {
 	if descOrder {
 		ipranges := poolIPSet.Ranges()
-		for i := range len(ipranges) - 1 {
+		for i := range len(ipranges) {
 			iprange := ipranges[len(ipranges)-1-i]
 			ip := iprange.To()
 			for {