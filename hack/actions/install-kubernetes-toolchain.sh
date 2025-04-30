aceeb79079b689d19f6be201765f336347c65475
Lubron <lubronzhan@gmail.com>
2025-04-30 14:38:02 -0700

    🌱 Kind node image to 1.33, kind to 0.27, kubectl to 1.33 (#222)
    * Bump kind node image for testing 1.33
    * Bump kind and kubectl
    
    Signed-off-by: Lubron Zhan <lubronzhan@gmail.com>

--- hack/actions/install-kubernetes-toolchain.sh
+++ hack/actions/install-kubernetes-toolchain.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly KUBECTL_VERS="v1.29.2"
-readonly KIND_VERS="v0.22.0"
+readonly KUBECTL_VERS="v1.33.0"
+readonly KIND_VERS="v0.27.0"
 
 readonly PROGNAME=$(basename $0)
 readonly CURL=${CURL:-curl}
@@ -14,38 +14,38 @@ readonly CURL=${CURL:-curl}
 readonly OS=$(uname | tr '[:upper:]' '[:lower:]')
 
 usage() {
-  echo "Usage: $PROGNAME INSTALLDIR"
+    echo "Usage: $PROGNAME INSTALLDIR"
 }
 
 download() {
     local -r url="$1"
     local -r target="$2"
-
+    
     echo Downloading "$target" from "$url"
     ${CURL} --progress-bar --location  --output "$target" "$url"
 }
 
 case "$#" in
-  "1")
-    mkdir -p "$1"
-    readonly DESTDIR=$(cd "$1" && pwd)
+    "1")
+        mkdir -p "$1"
+        readonly DESTDIR=$(cd "$1" && pwd)
     ;;
-  *)
-    usage
-    exit 64
+    *)
+        usage
+        exit 64
     ;;
 esac
 
 echo "Installing Kubernetes toolchain..."
 
 download \
-   "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERS}/kind-${OS}-amd64" \
-   "${DESTDIR}/kind"
+"https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERS}/kind-${OS}-amd64" \
+"${DESTDIR}/kind"
 
 chmod +x  "${DESTDIR}/kind"
 
 download \
-    "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERS}/bin/${OS}/amd64/kubectl" \
-    "${DESTDIR}/kubectl"
+"https://dl.k8s.io/release/${KUBECTL_VERS}/bin/${OS}/amd64/kubectl" \
+"${DESTDIR}/kubectl"
 
 chmod +x "${DESTDIR}/kubectl"
--- test/scripts/make-kind-cluster.sh
+++ test/scripts/make-kind-cluster.sh
@@ -9,7 +9,7 @@ set -o nounset
 readonly KIND=${KIND:-kind}
 readonly KUBECTL=${KUBECTL:-kubectl}
 
-readonly NODEIMAGE=${NODEIMAGE:-"kindest/node:v1.29.2@sha256:51a1434a5397193442f0be2a297b488b6c919ce8a3931be0ce822606ea5ca245"}
+readonly NODEIMAGE=${NODEIMAGE:-"kindest/node:v1.33.0@sha256:02f73d6ae3f11ad5d543f16736a2cb2a63a300ad60e81dac22099b0b04784a4e"}
 readonly CLUSTERNAME=${CLUSTERNAME:-kvcp-e2e}
 readonly WAITTIME=${WAITTIME:-5m}
 
@@ -22,15 +22,15 @@ kind::cluster::exists() {
 
 kind::cluster::create() {
     ${KIND} create cluster \
-        --name "${CLUSTERNAME}" \
-        --image "${NODEIMAGE}" \
-        --wait "${WAITTIME}"
+    --name "${CLUSTERNAME}" \
+    --image "${NODEIMAGE}" \
+    --wait "${WAITTIME}"
 }
 
 kind::cluster::load() {
     ${KIND} load docker-image \
-        --name "${CLUSTERNAME}" \
-        "$@"
+    --name "${CLUSTERNAME}" \
+    "$@"
 }
 
 if kind::cluster::exists "$CLUSTERNAME" ; then
@@ -40,8 +40,8 @@ fi
 
 # Create a fresh kind cluster.
 if ! kind::cluster::exists "$CLUSTERNAME" ; then
-  kind::cluster::create
-
-  # Print the k8s version for verification.
-  ${KUBECTL} version
+    kind::cluster::create
+    
+    # Print the k8s version for verification.
+    ${KUBECTL} version
 fi
